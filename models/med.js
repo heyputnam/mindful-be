538c749bda843374ab2029b8b141fc0bccbdd44d
heyputnam <heyputnam@gmail.com>
2021-04-07 20:08:01 -0400

    end of day 1

--- models/med.js
+++ models/med.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema;
 
 
 const medSchema = new Schema({
-    user: String,
-    name: String,
-    dose: String,
-    dateFill: Date,
-    // morning: Boolean,
-    // afternoon: Boolean,
-    // night: Boolean,
-
-}, {timestamps:true})
+        user: String,
+        name: String,
+        dose: String,
+        dateFill: Date,
+        morning:Boolean,
+        afternoon: Boolean,
+        night: Boolean
+    
+    }, {timestamps:true})
 
 
 module.exports = mongoose.model('Med', medSchema)
